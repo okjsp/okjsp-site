cae8efe9361805a54a3d9d492c3cb94ecf6794de
k-hwayoung <k-hwayoung@1400446e-9ab3-46cc-91c4-886daca6c3f0>
2010-03-17 01:56:25 +0000

    20100317_piki_게시판 글쓰기에서 한글 입력 오류 수정_서블릿파일 별도 분리
    git-svn-id: svn://okjsp.pe.kr/okjsp/trunk/okjsp2007@127 1400446e-9ab3-46cc-91c4-886daca6c3f0

--- src/kr/pe/okjsp/WriteServlet.java
+++ src/kr/pe/okjsp/WriteServlet.java
@@ -34,12 +34,7 @@ public class WriteServlet extends HttpServlet {
 	    	bbs = writeWithFiles(req, res);
 	    }
 	    
-	    //글 작성뒤에 모바일과 웹 구분_piki
-	    //글 작성 완료뒤 화면을 어디로 이동해야 할지 고민....
-	    boolean isMobileView = CommonUtil.nchk(req.getParameter("mobileView"),"N").equals("Y");
-	    //String togo = (isMobileView) ? "/bbs?act=MLIST&bbs="+bbs : "/bbs?act=LIST&bbs=" + bbs + "&pg=0";
-	    String togo = (isMobileView) ? "/m" : "/bbs?act=LIST&bbs=" + bbs + "&pg=0";
-		//String togo = "/bbs?act=LIST&bbs=" + bbs + "&pg=0";
+		String togo = "/bbs?act=LIST&bbs=" + bbs + "&pg=0";
 		res.sendRedirect(togo);
 
 	} // end doPost()
@@ -50,6 +45,7 @@ public class WriteServlet extends HttpServlet {
 		Article article = new Article();
 		article.setId(id);
 		article.setSid(sid);
+
 		String writer = req.getParameter("writer");
 		String bbs = req.getParameter("bbs");
 		String content = req.getParameter("content");
