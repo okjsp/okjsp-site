7a8753e5d82a71881cc496faf2ee020dfdd600e7
zxrvsm <zxrvsm@1400446e-9ab3-46cc-91c4-886daca6c3f0>
2010-09-29 08:36:18 +0000

    죄송합니다. 실수로 임포트 시켰네요;
    git-svn-id: svn://okjsp.pe.kr/okjsp/trunk/okjsp2007@921 1400446e-9ab3-46cc-91c4-886daca6c3f0

--- android/src/kr/pe/okjsp/android/OnScrollEndListener.java
+++
@@ -1,9 +0,0 @@
-package kr.pe.okjsp.android;
-
-public interface OnScrollEndListener
-{
-	public boolean isProcess();
-	public boolean isEnded();
-	public void setProcess(boolean isProcess);
-	public void onEndProcess();
-}
