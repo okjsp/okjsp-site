80e49b66dc2c76d1d6c7a05f8d572b0bcca1334b
kenu <kenu@1400446e-9ab3-46cc-91c4-886daca6c3f0>
2011-02-06 23:43:51 +0000

    sql
    git-svn-id: svn://okjsp.pe.kr/okjsp/trunk/okjsp2007@1018 1400446e-9ab3-46cc-91c4-886daca6c3f0

---
+++ web/WEB-INF/sql/okjsp2011.sql
@@ -0,0 +1,10 @@
+select count(*) FROM okboard WHERE bbsid = 'recruit' and id = 'kenu1' and wtime > (sysdate - 2)
+
+
+select sysdate, (sysdate - 7), a.* FROM okboard a
+WHERE bbsid = 'recruit' and id = 'kenu1'
+and wtime > (sysdate - 2)
+ORDER BY seq desc 
+for orderby_num() between 1 and 100
+
+select * from okboard_memo ORDER BY seq desc for orderby_num() between 1 and 100
---
+++ web/WEB-INF/sql/okmember.sql
@@ -0,0 +1,1 @@
+select name,email from okmember where mailing = 'Y';
