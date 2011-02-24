d85e5d0014e0a8bfac926016ca51872627fe00ea
kenu <kenu@1400446e-9ab3-46cc-91c4-886daca6c3f0>
2011-02-24 08:12:29 +0000

    데브멘토 듀오백
    git-svn-id: svn://okjsp.pe.kr/okjsp/trunk/okjsp2007@1042 1400446e-9ab3-46cc-91c4-886daca6c3f0

--- web/js/banner.js
+++ web/js/banner.js
@@ -1,11 +1,10 @@
 var Banner = {
 main : [
 {
-tag:'<img src="/images/banner/devmento_banner_468x60.gif" usemap="#seminar_banner_Map">' +
-	'<map name="seminar_banner_Map">' +
-	'<area shape="rect" alt="데브멘토 세미나 모바일 서비스 디자인 & 개발" coords="0,0,233,60" href="/f.jsp?http://www.devmento.co.kr/devmain/seminar/edumento_detail.jsp?dataSeq=89" target="_new">' +
-	'<area shape="rect" alt="데브멘토 세미나 구글 앱 엔진과 모바일 개발" coords="233,0,468,60" href="/f.jsp?http://www.devmento.co.kr/devmain/seminar/edumento_detail.jsp?dataSeq=90" target="_new">' +
-	'</map>'
+tag: '<a href="/f.jsp?http://www.devmento.co.kr/devmain/shop/shop_list.jsp" target="_blank">' +
+'<img src="/images/banner/devmento_duoback_478x93.gif" ' +
+'alt="어린이 공학 가방 듀오백 세트">' +
+'</a>'
 },
 {
 tag: '<a href="/f.jsp?http://www.devlec.com/?_pageVariable=OKJSP" target="_blank">' +
