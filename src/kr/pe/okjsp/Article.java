9f312d4c2743adedec422965838940e99e69c381
kenu <kenu@1400446e-9ab3-46cc-91c4-886daca6c3f0>
2011-02-25 14:35:50 +0000

    content linker
    git-svn-id: svn://okjsp.pe.kr/okjsp/trunk/okjsp2007@1046 1400446e-9ab3-46cc-91c4-886daca6c3f0

--- src/kr/pe/okjsp/Article.java
+++ src/kr/pe/okjsp/Article.java
@@ -388,13 +388,13 @@ public class Article {
 
 	public String getContentView() {
 		if( "1".equals(this.html) ) {
-			return CommonUtil.rplc(this.content, "<", "&lt;");
+			return HttpLinker.getLinkedSource(CommonUtil.rplc(this.content, "<", "&lt;"));
 		} else if( "0".equals(this.html) ) {
-			return CommonUtil.showHtml(this.content);
+			return HttpLinker.getLinkedSource(CommonUtil.showHtml(this.content));
 		}
 		return HttpLinker.getLinkedSource(this.content);
 	}
-
+	
 	/**
 	 * @return
 	 * @uml.property  name="password"
