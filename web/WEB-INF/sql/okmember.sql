524148adfc6cc5d419a8027b8edfad1dafe84062
kenu <kenu@1400446e-9ab3-46cc-91c4-886daca6c3f0>
2011-03-04 15:06:54 +0000

    test
    git-svn-id: svn://okjsp.pe.kr/okjsp/trunk/okjsp2007@1060 1400446e-9ab3-46cc-91c4-886daca6c3f0

--- web/WEB-INF/sql/okmember.sql
+++ web/WEB-INF/sql/okmember.sql
@@ -1,1 +1,10 @@
 select name,email from okmember where mailing = 'Y';
+
+
+select count(*) cnt, max(sid) from okmember;
+
+update okmember set mailing = 'N' where email in (
+'a@orea.com'
+);
+
+select name,email from okmember where mailing = 'Y' order by sid;
