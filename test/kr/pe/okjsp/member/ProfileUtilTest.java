07680fbd8d5d32a112e318de94efd5ca47327177
kenu <kenu@1400446e-9ab3-46cc-91c4-886daca6c3f0>
2011-04-04 15:38:42 +0000

    profile
    git-svn-id: svn://okjsp.pe.kr/okjsp/trunk/okjsp2007@1092 1400446e-9ab3-46cc-91c4-886daca6c3f0

--- test/kr/pe/okjsp/member/ProfileUtilTest.java
+++ test/kr/pe/okjsp/member/ProfileUtilTest.java
@@ -9,7 +9,7 @@ public class ProfileUtilTest extends TestCase {
 	ProfileUtil profileUtil = new ProfileUtil();
 	public void testCopy() throws Exception {
 		String basePath = "web/profile/base/";
-		String path = "web/profiles/";
+		String path = "web/profile/";
 		String sourceImage = basePath + ProfileUtil.defaultName;
 		String destImage = path + ProfileUtil.defaultName;
 		assertTrue(new File(sourceImage).exists());
