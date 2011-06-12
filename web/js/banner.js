d3143a98b079d4d1cf38dab5ec18b8542c1893f9
kenu <kenu@1400446e-9ab3-46cc-91c4-886daca6c3f0>
2011-06-12 07:36:47 +0000

    공개 소프트웨어 ~7/13
    git-svn-id: svn://okjsp.pe.kr/okjsp/trunk/okjsp2007@1172 1400446e-9ab3-46cc-91c4-886daca6c3f0

--- web/js/banner.js
+++ web/js/banner.js
@@ -40,24 +40,35 @@ footer : [
 	'<img src="/images/banner/raction_468x60.png" alt="공짜 가라사대 랙션하라"></a>'
 }
 ],
+
+aside : [
+ {
+tag : "<li>"+
+"<a href=\"/f.jsp?http://project.oss.kr/?okjsp\" target=\"_blank\" >"+
+"<img src=\"/images/banner/opensw_134x40.gif\" "+
+"	alt=\"2011 공개SW개발자대회\">"+
+"</a>"+
+"</li>"
+ },
+ {
+tag : "<li>"+
+ "<a href=\"/f.jsp?http://cs.ucloud.com\" target=\"_blank\" >"+
+ "<img src=\"/images/banner/ucloud_134x40.gif\" "+
+ "	alt=\"KT Ucloud\" ></a>"+
+ "</li>"
+ }
+],
 show : function() {
 	var idx = Math.floor(Math.random() * this.main.length);
 	document.write(this.main[idx].tag);
 },
 
 showAside : function() {
-	document.write("<ul>" +
-			"<li>"+
-			"<a href=\"/f.jsp?http://www.apptalk.tv\" target=\"_blank\" >"+
-			"<img src=\"/images/banner/apptalk_134x60.jpg\" "+
-			"	alt=\"세상의 모든 앱을 영상으로. 앱의 모든 것, 앱톡\">"+
-			"</a>"+
-			"</li>"+
-			"<li>"+
-			"<a href=\"/f.jsp?http://cs.ucloud.com\" target=\"_blank\" >"+
-			"<img src=\"/images/banner/ucloud_134x40.gif\" "+
-			"	alt=\"KT Ucloud\" ></a>"+
-			"</li></ul>");
+	document.writeln("<ul>");
+	for(var i = 0; i < this.aside.length; i++) {
+		document.writeln(this.aside[i].tag);
+	}
+	document.writeln("</ul>");
 },
 
 showLeftSide : function() {
