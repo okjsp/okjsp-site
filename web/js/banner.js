2a99127cd72dc7ab02c92c5adc5c1d5571776068
kenu <kenu@1400446e-9ab3-46cc-91c4-886daca6c3f0>
2011-06-17 05:03:09 +0000

    apptalk ~7/18
    git-svn-id: svn://okjsp.pe.kr/okjsp/trunk/okjsp2007@1183 1400446e-9ab3-46cc-91c4-886daca6c3f0

--- web/js/banner.js
+++ web/js/banner.js
@@ -1,10 +1,16 @@
 var Banner = {
 main : [
 {
-tag: '<a href="/f.jsp?http://www.devlec.com/?_pageVariable=OKJSP" target="_blank">' +
-'<img src="http://www.devlec.com/images/devlec_okjsp.gif" ' +
+tag: '<a href="/f.jsp?http://android.apptalk.co.kr/apptalk_business" target="_blank">' +
+'<img src="/images/banner/apptalk_473x98.gif" ' +
 'alt="▒▒▒ 데브렉 ▒▒▒ 개발자를 위한 국내 최고 프로그래밍 전문 동영상강좌 사이트">' +
 '</a>'
+},
+{
+	tag: '<a href="/f.jsp?http://www.devlec.com/?_pageVariable=OKJSP" target="_blank">' +
+	'<img src="http://www.devlec.com/images/devlec_okjsp.gif" ' +
+	'alt="▒▒▒ 데브렉 ▒▒▒ 개발자를 위한 국내 최고 프로그래밍 전문 동영상강좌 사이트">' +
+	'</a>'
 }
 ],
 
