8c4e06be22f4831ec8dcdb6eec75e8d081558ffc
kenu <kenu@okjsp.pe.kr>
2011-08-29 15:27:00 +0900

    banner 정리/tomcat7 api implements 추가/.classpath/egov

--- test/kr/pe/okjsp/util/HttpServletRequestBox.java
+++ test/kr/pe/okjsp/util/HttpServletRequestBox.java
@@ -4,16 +4,25 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.Principal;
+import java.util.Collection;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
+import javax.servlet.AsyncContext;
+import javax.servlet.DispatcherType;
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.ServletInputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
 
 public class HttpServletRequestBox implements HttpServletRequest {
 	private Map params = new HashMap();
@@ -289,6 +298,81 @@ public class HttpServletRequestBox implements HttpServletRequest {
 	public int getRemotePort() {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+
+	@Override
+	public AsyncContext getAsyncContext() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DispatcherType getDispatcherType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ServletContext getServletContext() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAsyncStarted() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAsyncSupported() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public AsyncContext startAsync() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AsyncContext startAsync(ServletRequest arg0, ServletResponse arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean authenticate(HttpServletResponse arg0) throws IOException,
+			ServletException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Part getPart(String arg0) throws IOException, IllegalStateException,
+			ServletException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Part> getParts() throws IOException,
+			IllegalStateException, ServletException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void login(String arg0, String arg1) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void logout() throws ServletException {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- web/js/banner.js
+++ web/js/banner.js
@@ -1,11 +1,6 @@
 var Banner = {
 main : [
 {
-tag: '<a href="/f.jsp?http://www.choongang.co.kr/html/m0_s1_4_s1.asp?id=26200" target="_blank">' +
-'<img src="/images/banner/choongang_473x98.gif" ' +
-'alt="국비지원 IT 취업연수생 모집"></a>'
-},
-{
     tag: '<a href="/f.jsp?http://www.devlec.com/?_pageVariable=OKJSP" target="_blank">' +
     '<img src="http://www.devlec.com/images/devlec_okjsp.gif" ' +
     'alt="▒▒▒ 데브렉 ▒▒▒ 개발자를 위한 국내 최고 프로그래밍 전문 동영상강좌 사이트"></a>'
@@ -16,20 +11,11 @@ contentRight : [
 ],
 
 content : [
-{
-    tag: '<a href="/f.jsp?http://www.gsion.com/?okjsp" target="_blank">' +
-    '<img src="http://www.GSION.com/Files/NIF/201107/jsp배너.JPG" ' +
-    'alt="정보처리기술사의 모든 것 지아이에스에서 확인하세요!"></a>'
-}
-           
 ],
 
 left : [
 {
     tag: '<li><A href="/f.jsp?http://www.innorix.com/DS/" title="대용량 파일/폴더 업로드 컴포넌트" target="innorix">업로드는 InnoDS</A></li>'
-},
-{
-    tag: '<li><A href="/f.jsp?http://okmindmap.com/" title="웹기반 협업 마인드맵, OKJSP 유저 박기원님이 참여한 서비스" target="imaso">okmindmap</A></li>'
 },
 {
     tag: '<li><A href="/f.jsp?http://bit.ly/cIQ7fA" title="안드로이드 개발환경 구축" target="android">[유료]안드로이드 개발매뉴얼1</A></li>'
@@ -43,14 +29,6 @@ footer : [
 ],
 
 aside : [
- {
-tag : "<li>"+
-"<a href=\"/f.jsp?http://www.devmento.co.kr\" target=\"_blank\" >"+
-"<img src=\"/images/banner/devmento_134x60.gif\" "+
-"   alt=\"개발자들의 지식공유 쉼터 데브멘토\">"+
-"</a>"+
-"</li>"
- },
  {
  tag : "<li>"+
  "<a href=\"/f.jsp?http://project.oss.kr/?okjsp\" target=\"_blank\" >"+
