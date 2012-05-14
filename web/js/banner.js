31a4b2dcd88286d411de8e8173a6f0dc60cd45b2
kenu <kenu.heo@gmail.com>
2012-05-15 06:44:43 +0900

    탭스랩 종료

--- web/js/banner.js
+++ web/js/banner.js
@@ -2,7 +2,7 @@ var Banner = {
 main : [
 {
 	tag: '<a href="/f.jsp?http://sda.re.kr/bbs/board.php?bo_table=festival_contents&wr_id=1" target="_blank">' +
-	'<img src="/images/banner/sda_conf_940x280.jpg" ' +
+	'<img src="/images/banner/sda_conf_940x280.jpg" style="width:473px;height:98px" ' +
 	'alt="글로벌 커뮤니티 서미트 2012"></a>'
 },
 {
@@ -44,10 +44,6 @@ footer : [
 ],
 
 aside : [
-{
-	tag: '<a href="/f.jsp?http://www.tabslab.com/kr/">' +
-	'<img src="/images/banner/tabscf_150x67.gif" alt="탭스 업로드 JAVA"></a>'
-},
 {
 	tag: '<a href="/f.jsp?http://project.oss.kr/">' +
 	'<img src="/images/banner/oss_project_150x67.gif" alt="OSS World Challenge 2012, 6th 공개소프트웨어 개발자 대회"></a>'
@@ -123,91 +119,3 @@ showFooter : function() {
 
 };
 
-//배열선언 type, image, url
-//가족 배너
-var banner = new Array(
-"0,,/images/bnr_koug.gif,,http://www.koug.net/,,88,,31"
-);
-
-//상업용 배너 , 친구네 배너
-var bannerSponsored = new Array(
-//"0,,http://whiteto.img.or.kr/images/banner/banner_05.gif,,/f.jsp?http://www.jineejinee.com/,,468,,60,,처음 okjsp를 같이 운영했던 친구네 옷가게입니다."
-);
-
-//일반 배너
-var banner2 = new Array(
-"0,,/images/doubleiqlogo.gif,,http://doubleiq.pe.kr/,,88,,31",
-"0,,/images/bnr_javaclub.gif,,http://cafe.naver.com/javacircle.cafe,,88,,31",
-"0,,/images/bnr_javajigi.gif,,http://www.javajigi.net/,,88,,31",
-"1,,/images/bnr_javacafe.swf,,http://www.javacafe.or.kr/,,88,,31"
-);
-
-//고정 배너
-var bannerDb = new Array(
-"0,,/images/bnr_simples.png,,/f.jsp?http://simples.kr/,,88,,31",
-"0,,/images/bnr_itnodong.png,,/f.jsp?http://it.nodong.net/,,88,,31",
-"0,,/images/bnr_acornpub_88x31.gif,,/f.jsp?http://www.acornpub.co.kr/blog/,,88,,31",
-"0,,/images/bnr_oracleclub.gif,,http://www.oracleclub.com/,,88,,31",
-"0,,http://apmtip.com/images/banner/apmtip.gif,,http://apmtip.com/,,88,,31",
-"0,,http://www.kkaok.pe.kr/images/kkaokon.gif,,http://www.kkaok.pe.kr/,,88,,31",
-"0,,/images/hanajava.gif,,http://www.hanajava.net/,,88,,31",
-"0,,http://angel.sarang.net/images/angel.gif,,http://angel.sarang.net/,,88,,31",
-"0,,http://www.jazzvm.net/banners/jazzvm.gif,,http://www.jazzvm.net/,,88,,31"
-);
-
-//나만 보는 배너
-var bannerHidden = new Array(
-"0,,/images/bnr_nettop.gif,,http://www.nettop.pe.kr/,,88,,31",
-"0,,http://www.j2eestudy.co.kr/banner/J2ee_logo_small.jpg,,http://www.j2eestudy.co.kr/,,88,,31",
-"0,,/images/bnr_openaudio_small.gif,,/f.jsp?http://www.openaudio.co.kr/,,88,,31",
-"0,,/images/bnr_jongsclub.gif,,http://www.jongsclub.com/,,88,,31",
-"0,,/images/bnr_theserverside.gif,,http://www.theserverside.com/,,88,,31",
-"0,,http://www.javaservice.net/~java/bbs/images/banner/jsn88x31.gif,,http://www.javaservice.net/,,88,,31",
-"0,,/images/bnr_openseed.gif,,http://openseed.net/,,88,,31",
-"0,,/images/jspinsider.gif,,http://www.jspinsider.com/,,88,,31",
-"0,,/images/bnr_sfnet.gif,,http://sf.net/,,88,,31",
-
-
-"0,,/images/bnr_bizcom.gif,,http://bizcom.kookmin.ac.kr/",
-"0,,http://www.jspschool.com/images/logos.gif,,http://www.jspschool.com/",
-"0,,http://www.javasun.net/img/javasun.gif,,http://www.javasun.net/",
-"0,,/images/bnr_jspnet.gif,,http://www.neoburi.com/",
-
-"0,,/images/HIMTON_BANNER2.jpg,,http://www.himton.com/,,88,,31",
-"0,,http://www.flex.or.kr/images/banner/bnr_flexorkr_8831.gif,,http://www.flex.or.kr/,,88,,31",
-"0,,http://aboutjsp.com/image/banner.gif,,http://www.aboutjsp.com/",
-"0,,/images/bnr_salthome.gif,,http://www.salthome.com/,,jboss정보",
-"0,,/images/bnr_javapattern.gif,,http://www.javapattern.info/",
-"0,,/images/4OffLine.gif,,http://www.4offline.org/",
-"0,,/images/bnr_javastudy.gif,,http://www.javastudy.co.kr/",
-"0,,/images/bnr_jlab.gif,,http://www.jlab.net/",
-"1,,http://www.kimho.pe.kr/khbanner/kimhologo.swf,,http://www.kimho.pe.kr/,,88,,31",
-"0,,/images/bnr_iasandcb.gif,,http://www.iasandcb.pe.kr/",
-"0,,/images/bnr_bawoo.gif,,http://www.bawoo.pe.kr/",
-"0,,/images/bnr_50001.gif,,http://www.50001.com/",
-"0,,/images/konanlogo.jpg,,http://www.sqler.pe.kr/",
-
-"1,,http://www.taeyo.pe.kr/main/youngji/taeyo_newwin.swf,,88,,31",
-"0,,http://www.superuser.co.kr/main/rogo.gif,,http://www.superuser.co.kr/",
-"0,,/images/kldp.gif,,http://www.kldp.org/",
-
-"0,,/images/bnr_kmoon.gif,,http://www.kmoon.co.kr"
-);
-/*
-보류중인 배너
-"0,,http://whiteto.img.or.kr/images/banner/banner_06.gif,,/f.jsp?http://www.jineejinee.com/,,88,,32",
-"0,,/images/jspmaster.gif,,http://www.jspmaster.com/",
-"0,,/images/fontpiabn3.gif,,http://www.fontpia.co.kr/",
-"0,,/images/bnr_suribada.gif,,http://www.suribada.com/",
-"0,,/images/bnr_ehanbit.gif,,http://www.ehanbit.net/",
-"0,,/images/bnr_pohangsi.gif,,http://www.pohangsi.com/",
-"0,,http://steelheart.pe.kr/image/banner/steelheart.pe.kr.gif,,http://steelheart.pe.kr/",
-"0,,/images/bnr_developerclub.gif,,http://www.developerclub.com/"
-"0,,http://javacool.net/image/banner/javacool1.gif,,http://javacool.net/",
-"0,,/images/bnr_javacraft.gif,,http://www.freechal.com/ejbstudy/"
-"0,,/images/bnr_javacan.gif,,http://javateam.korea.ac.kr/javacan/",
-"1,,http://doubleiq.pe.kr/img/lastcom_banner.swf",
-"0,,http://www.java2java.com/images/banner/java2java.gif,,http://www.java2java.com/",
-"0,,http://211.187.137.221:9999/images/oracleib.gif,,http://www.oraclei.net/",
-"0,,/images/bnr_mute.gif,,http://www.mutecast.net/"
-*/
