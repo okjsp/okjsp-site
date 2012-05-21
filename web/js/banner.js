26a204949d9357b46418c557dc1808f933e6256e
kenu <kenu.heo@gmail.com>
2012-05-21 09:49:59 +0900

    Friend of Eclipse ~2013/05/14

--- web/js/banner.js
+++ web/js/banner.js
@@ -65,6 +65,7 @@ show : function() {
 },
 
 showAside : function() {
+	document.writeln('<a href="http://www.eclipse.org/donate/" target="_blank"><img src="http://eclipse.org/donate/images/friendslogo200.jpg" style="width:150px"></a>');
     document.writeln("<ul>");
     for(var i = 0; i < this.aside.length; i++) {
         document.writeln("<li>"+this.aside[i].tag+"</li>");
