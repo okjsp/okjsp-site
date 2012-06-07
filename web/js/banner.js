73de97448c18d86597980377d583eefae35eaf55
kenu <kenu.heo@gmail.com>
2012-06-07 12:58:32 +0900

    자바카페 오픈세미나 ~6/16

--- web/js/banner.js
+++ web/js/banner.js
@@ -48,12 +48,12 @@ aside : [
 	'<img src="/images/banner/olc_model_150x67.gif" alt="Spring 3.1/Script Language 과정 OLC"></a>'
 },
 {
-	tag: '<a href="/f.jsp?http://hanbitedu.or.kr/curriculum/programming.html">' +
-	'<img src="/images/banner/hbilab_150x67.gif" alt="자바개발자 양성과정"></a>'
+	tag: '<a href="/f.jsp?http://javacafe.or.kr/board/view/BBS_ID/NTC_001/ATC_NO/91">' +
+	'<img src="/images/banner/javacafe_seminar_150x67.jpg" alt="자바카페 공개 세미나 2012"></a>'
 },
 {
-	tag: '<a href="/f.jsp?http://project.oss.kr/">' +
-	'<img src="/images/banner/oss_project_150x67.gif" alt="OSS World Challenge 2012, 6th 공개소프트웨어 개발자 대회"></a>'
+	tag: '<a href="/f.jsp?http://hanbitedu.or.kr/curriculum/programming.html">' +
+	'<img src="/images/banner/hbilab_150x67.gif" alt="자바개발자 양성과정"></a>'
 },
 {
 	tag: '<a href="/f.jsp?http://dev.ucloud.com/">' +
