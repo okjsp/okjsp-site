be4523003327e61b88bd7056e806a6f01891fab4
kenu <kenu.heo@gmail.com>
2012-10-21 00:42:27 +0900

    19765

--- src/kr/pe/okjsp/util/Spam.java
+++ src/kr/pe/okjsp/util/Spam.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Spam {
 
 	public static void checkSpammer(long sid) throws IOException {
-		if  (sid == 17004 || sid == 19384) {
+		if  (sid == 19765 || sid == 17004 || sid == 19384) {
 			throw new IOException("NO RIGHT TO USE!!!");
 		}
 	}
