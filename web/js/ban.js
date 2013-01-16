305232e57eb6720679c4be6e36156205a6b89c87
kenu <kenu.heo@gmail.com>
2013-01-17 02:10:25 +0900

    ban rkdrkdskarn

--- web/js/ban.js
+++ web/js/ban.js
@@ -1,6 +1,6 @@
 // 금지단어
 var ban = new Array(
-'카지노', 'qpr1000', '경마', 'imc100', 'dmlover', 'first-shop', 'first-dm'
+'카지노', 'qpr1000', '경마', 'imc100', 'dmlover', 'first-shop', 'first-dm', 'rkdrkdskarn'
 );
 // 금지단어 판단
 function filterWord(content) {
