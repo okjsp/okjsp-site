5c4605e596dc97eaff20b77d40ba97372107fe62
kenu <kenu.heo@gmail.com>
2013-03-04 13:39:04 +0900

    ex member bug

--- src/main/java/kr/pe/okjsp/util/Spam.java
+++ src/main/java/kr/pe/okjsp/util/Spam.java
@@ -5,7 +5,8 @@ import java.io.IOException;
 public class Spam {
 
 	public static void checkSpammer(long sid) throws IOException {
-		if  (sid == 20323 || sid == 19765 || sid == 17004 || sid == 19384|| sid == 19826) {
+		if  (sid == 21100 || sid == 20323 
+				|| sid == 19765 || sid == 17004 || sid == 19384|| sid == 19826) {
 			throw new IOException("NO RIGHT TO USE!!!");
 		}
 	}
