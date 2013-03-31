d884b15839088fa5b0d146ce6461d87e81f6cd74
kenu <kenu.heo@gmail.com>
2013-03-31 16:36:59 +0900

    JRE

--- src/main/webapp/WEB-INF/sql/okboard_info.sql
+++ src/main/webapp/WEB-INF/sql/okboard_info.sql
@@ -2,3 +2,7 @@ select * from okboard_info;
 
 insert into okboard_info (cseq, bbsid, name, header) 
 values (1, 'pr', '홍보', '행사, 제품, 스터디 등을 널리 알리는 곳입니다.');
+
+select * from okboard_info where bbsid = 'bbs4';
+
+update okboard_info set name = 'Java/JSP Tips' where bbsid = 'bbs4';
