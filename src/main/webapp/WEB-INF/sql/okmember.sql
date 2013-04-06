6cfac21f21dc71bcf6ffe5fe169d9ec1c78727b9
kenu <kenu.heo@gmail.com>
2013-04-06 15:44:52 +0900

    okmember

--- src/main/webapp/WEB-INF/sql/okmember.sql
+++ src/main/webapp/WEB-INF/sql/okmember.sql
@@ -18,16 +18,20 @@ select * from okmember where id like 'sbroh%';
 select * from okrole where id like 'mangsang%';
 
 delete from okrole where id = 'mangsang00';
-select * from okmember where email like 'loger%';
 
-update okmember set email = 'yjkim_king@lycos.co.kr' where id = 'gatnet19';
-update okmember set joindate = '2011-10-28 17:15:27' where id = 'gatnet19';
+select * from okmember where email like 'loger%';
+select * from okmember where sid = 20173;
+
+update okmember set email = 'y@lycos.co.kr' where id = 'y';
+update okmember set joindate = '2011-10-28 17:15:27' where id = 'y';
+
+select * from okmember where joindate = '1970-01-02 00:00:00' limit 10;
+
+select count(*) from okmember where joindate = '1970-01-02 00:00:00';
+-- 1450
+update okmember set joindate = '2011-09-18 01:23:32' where joindate = '1970-01-02 00:00:00';
+-- 2011-01-13 01:23:32 to 2011-09-18 01:23:32
 
 select * from point_history where sid = 15388;
 
-
 select * from okmember order by sid;
-
- kenu1 2f510dbf079b04a2 ÄÉ´©   okjspgwt@gmail.com 2007-10-08 15:02:28.0 okjsp    NULL    Y       N           0 3582
-
-select * from okmember where sid = 20173;
