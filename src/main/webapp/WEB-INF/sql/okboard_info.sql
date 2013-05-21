1828ea475cfa448b5fbbffa62abfca46b4f6c7f5
kenu <kenu.heo@gmail.com>
2013-05-21 16:50:49 +0900

    cooper

--- src/main/webapp/WEB-INF/sql/okboard_info.sql
+++ src/main/webapp/WEB-INF/sql/okboard_info.sql
@@ -1,7 +1,7 @@
 select * from okboard_info;
 
 insert into okboard_info (cseq, bbsid, name, header) 
-values (1, 'pr', '홍보', '행사, 제품, 스터디 등을 널리 알리는 곳입니다.');
+values (1, 'cooper', '협동조합준비위', '협동조합관련 정보를 널리 알리는 곳입니다.');
 
 select * from okboard_info where bbsid = 'bbs4';
 
