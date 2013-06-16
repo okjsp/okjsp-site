2469ed67169a365803e81af4810d42da028c0344
kenu <kenu.heo@gmail.com>
2013-06-16 20:14:10 +0900

    stat ad

--- src/main/webapp/WEB-INF/sql/okad_log.sql
+++ src/main/webapp/WEB-INF/sql/okad_log.sql
@@ -25,3 +25,11 @@ order by 1 desc, 3 desc
 ;
 
 select * from okad_log where credate between '2012-05-01' and '2012-05-02' order by url desc
+
+--
+select date_format(credate, '%Y-%m-%d') as ldate, 
+count(date_format(credate, '%Y-%m-%d')) as lcount 
+from okad_log 
+where credate between CAST(TO_DATE('201306','YYYYMM') AS TIMESTAMP)
+and CAST(TO_DATE('201307','YYYYMM') AS TIMESTAMP)
+group by date_format(credate, '%Y-%m-%d') order by 1 desc
