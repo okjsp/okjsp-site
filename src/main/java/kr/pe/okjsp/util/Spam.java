cf26e924af2e82f9d38d34e50c14e5588a244969
kwamuki <kwamuki@gmail.com>
2013-07-10 11:45:41 +0900

    spam.java 수정-sizers스팸등록

--- src/main/java/kr/pe/okjsp/util/Spam.java
+++ src/main/java/kr/pe/okjsp/util/Spam.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Spam {
 
 	public static void checkSpammer(long sid) throws IOException {
-		if  (sid == 21797) {
+		if  (sid == 21797 || sid == 6297) {
 			throw new IOException("NO RIGHT TO USE!!!");
 		}
 	}
