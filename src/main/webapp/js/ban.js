974ced58e4acc5e5338b804b3d936b5042aa3d4f
kenu <kenu.heo@gmail.com>
2013-09-27 00:01:34 +0900

    ban word

--- src/main/webapp/js/ban.js
+++ src/main/webapp/js/ban.js
@@ -1,7 +1,7 @@
 // 금지단어
 var ban = new Array(
-'중고차', '카지노', 'qpr1000', '경마', 'imc100', 'dmlover', 'first-shop', 'first-dm', 'rkdrkdskarn', 
-'jjr8858', '승부존', 'clippers', '부업', 'hyejin000', 'caspinmsn', '사내하청'
+'ab88', '카지노', 'qpr1000', '경마', 'imc100', 'dmlover', 'first-shop', 'first-dm', 'rkdrkdskarn', 
+'jjr8858', '택알바', '승부존', 'clippers', '부업', 'hyejin000', 'caspinmsn'
 );
 // 금지단어 판단
 function filterWord(content) {
