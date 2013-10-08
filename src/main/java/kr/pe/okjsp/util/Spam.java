2cb2c808d4e6576345f896108341484503391964
kwamuki <kwamuki@gmail.com>
2013-10-08 18:10:09 +0900

    CSS공부 스팸처리(막걸리)

--- src/main/java/kr/pe/okjsp/util/Spam.java
+++ src/main/java/kr/pe/okjsp/util/Spam.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Spam {
 
 	public static void checkSpammer(long sid) throws IOException {
-		if  (sid == 21797 || sid == 6297 || sid == 15548) {
+		if  (sid == 21797 || sid == 6297 || sid == 15548 || sid == 19847) {
 			throw new IOException("NO RIGHT TO USE!!!");
 		}
 	}
