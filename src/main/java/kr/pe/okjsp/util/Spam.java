58f9f212f89979c84d4f0819193596049115f51e
kwamuki <kwamuki@gmail.com>
2013-12-19 11:34:33 +0900

    스팸추가:챨리(막걸리)

--- src/main/java/kr/pe/okjsp/util/Spam.java
+++ src/main/java/kr/pe/okjsp/util/Spam.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Spam {
 
 	public static void checkSpammer(long sid) throws IOException {
-		if  (sid == 21797 || sid == 6297 || sid == 15548 || sid == 19847 || sid == 24706 || sid == 24830) {
+		if  (sid == 21797 || sid == 6297 || sid == 15548 || sid == 19847 || sid == 24706 || sid == 24830 || sid == 25663) {
 			throw new IOException("NO RIGHT TO USE!!!");
 		}
 	}
