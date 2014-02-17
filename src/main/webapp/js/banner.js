cc70aef91eb50c27a5b76d77f903100fdbc90a61
kwamuki <kwamuki@gmail.com>
2014-02-17 11:31:58 +0900

    에이체크(시도우) 배너광고 추가

--- src/main/webapp/js/banner.js
+++ src/main/webapp/js/banner.js
@@ -54,7 +54,8 @@ showAside : function() {
 	//document.writeln('<a href="/f.jsp?http://techplanet.skplanet.com" target="skp"><img src="/images/banner/banner_skp_131025.gif" style="width:160px"></a>');
 	//document.writeln('<a href="/f.jsp?http://open.egovframe.go.kr/projects/notices/event/8496" target="ebu"><img src="/images/banner/banner_superk_131001(150).jpg" style="width:150px"></a>');
 	//document.writeln('<a href="/f.jsp?http://www.hanb.co.kr/events/eventview.html?event_id=cleaninfo" target="hb"><img src="/images/banner/banner_hb_131021(175).jpg" style="width:160px"></a>');
-	document.writeln('<a href="/f.jsp?https://www.facebook.com/dailymaso?ref=ts&fref=ts" target="maso"><img src="/images/banner/banner_maso_131129(160).jpg" style="width:160px"></a>');
+	//document.writeln('<a href="/f.jsp?https://www.facebook.com/dailymaso?ref=ts&fref=ts" target="maso"><img src="/images/banner/banner_maso_131129(160).jpg" style="width:160px"></a>');
+	document.writeln('<a href="/f.jsp?http://www.acheck.co.kr/" target="cidow"><img src="/images/banner/banner_acheck_140217(160).jpg" style="width:160px"></a>');
 },
 
 showAsideBottom : function() {
