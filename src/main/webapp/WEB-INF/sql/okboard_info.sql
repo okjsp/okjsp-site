dbf6c6e2f0532ccace6927804a8648806292f070
kwamuki <kwamuki@gmail.com>
2014-06-23 17:28:02 +0900

    eGov추가

--- src/main/webapp/WEB-INF/sql/okboard_info.sql
+++ src/main/webapp/WEB-INF/sql/okboard_info.sql
@@ -1,8 +1,8 @@
 select * from okboard_info;
 
 insert into okboard_info (cseq, bbsid, name, header) 
-values (1, 'getjob', '구직', '직장을 원하는 프로그래머를 위한 게시판입니다.');
+values (1, 'egov', '전자정부프레임웍', '전자정부프레임웍 관련 게시판입니다.');
 
-select * from okboard_info where bbsid = 'bbs4';
+select * from okboard_info where bbsid = 'egov';
 
 update okboard_info set name = '구인' where bbsid = 'recruit';
