3b9654ca7a6d941f819a558d2e207030a20b7fa8
kwamuki <kwamuki@gmail.com>
2014-06-24 10:03:45 +0900

    eGov 명칭 수정

--- src/main/webapp/WEB-INF/sql/okboard_info.sql
+++ src/main/webapp/WEB-INF/sql/okboard_info.sql
@@ -1,8 +1,8 @@
 select * from okboard_info;
 
 insert into okboard_info (cseq, bbsid, name, header) 
-values (1, 'egov', '전자정부프레임웍', '전자정부프레임웍 관련 게시판입니다.');
+values (1, 'egov', '전자정부표준프레임워크', '전자정부 표준프레임워크 관련 게시판입니다.');
 
 select * from okboard_info where bbsid = 'egov';
 
-update okboard_info set name = '구인' where bbsid = 'recruit';
+update okboard_info set header = '전자정부 표준프레임워크 관련 게시판입니다.' where bbsid = 'egov';
