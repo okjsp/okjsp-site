834b915d1c0fd83ee4f3ad0074b5d13187d032bf
kwamuki <kwamuki@gmail.com>
2014-07-01 14:40:40 +0900

    OKJSP리뉴얼 게시판 쿼리작성

--- src/main/webapp/WEB-INF/sql/okboard_info.sql
+++ src/main/webapp/WEB-INF/sql/okboard_info.sql
@@ -1,8 +1,8 @@
 select * from okboard_info;
 
 insert into okboard_info (cseq, bbsid, name, header) 
-values (1, 'egov', '전자정부표준프레임워크', '전자정부 표준프레임워크 관련 게시판입니다.');
+values (1, 'okhee2', 'OKJSP리뉴얼', 'OKJSP 리뉴얼 프로젝트 관련 게시판입니다.');
 
-select * from okboard_info where bbsid = 'egov';
+select * from okboard_info where bbsid = 'okhee2';
 
 update okboard_info set header = '전자정부 표준프레임워크 관련 게시판입니다.' where bbsid = 'egov';
